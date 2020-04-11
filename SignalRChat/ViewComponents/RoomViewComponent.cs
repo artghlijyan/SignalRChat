fa18251ddbdec55f90d374b4a0d2388ddcc79053
Arthur Ghlijyan <deckuser@hotmail.com>
2020-04-11 15:22:16 +0400

    _

---
+++ SignalRChat/ViewComponents/RoomViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalRChat.DbRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.ViewComponents
+{
+    public class RoomViewComponent : ViewComponent
+    {
+        private AppDbContext _dbContext;
+
+        public RoomViewComponent(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var chats = _dbContext.Chats.ToList();
+            return View(chats);
+        }
+    }
+}
